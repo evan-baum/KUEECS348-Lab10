f152e198e5af5ce7557dbad0e29d394dc35899c6
Evan-Baumgartner <evan.baum@ku.edu>
2025-10-30 10:22:00 -0500

    init

---
+++ stringCalc.cpp
@@ -0,0 +1,19 @@
+#include <string>;
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class stringNum{
+    public:
+        string num;
+
+        int validNum() {
+            return 0;
+        }
+};
+
+
+
+int main() {
+    return 0;
+}
